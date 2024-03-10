c9a56a4d8b75fb377d9cbdaf4a32f37f3cf30f70
Omar Flores <43190119+omarfq@users.noreply.github.com>
2024-03-09 21:44:39 -0400

    Add -l flag to read lines to wc program

--- omarfq/firstProgram/wc/main.go
+++ omarfq/firstProgram/wc/main.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	// Calling the count function to count the number of words
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+
+	// Parsing the flags provided by the user
+	flag.Parse()
+
+	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count owrds so we define
+	// the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Define a counter
 	wc := 0
