53bfcae3fe4a65f9b8d2b1c030d5d4f7b989349f
Omar Flores <43190119+omarfq@users.noreply.github.com>
2024-03-09 22:24:59 -0400

    Add count bytes feature from exercise, add wc.exe binary for Windows

--- omarfq/firstProgram/wc/main.go
+++ omarfq/firstProgram/wc/main.go
@@ -12,22 +12,27 @@ func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
 
+	// Defining a boolean flag -b to count bytes
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
 	// If the count lines flag is not set, we want to count owrds so we define
 	// the scanner split type to words (default is split by lines)
-	if !countLines {
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Define a counter
--- omarfq/firstProgram/wc/main_test.go
+++ omarfq/firstProgram/wc/main_test.go
@@ -11,7 +11,7 @@ func TestCountWords(t *testing.T) {
 
 	exp := 4
 
-	res := count(b, false)
+	res := count(b, false, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -24,7 +24,20 @@ func TestCountLines(t *testing.T) {
 
 	exp := 3
 
-	res := count(b, true)
+	res := count(b, true, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountBytes tests the count function set to count bytes
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4\n")
+
+	exp := 24
+
+	res := count(b, false, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
