5b3ae17474134f97a937c84628fdded0c929747d
Omar Flores <43190119+omarfq@users.noreply.github.com>
2024-03-16 08:58:51 -0400

    Add ToDo API and tests

--- omarfq/2interacting/todo/todo.go
+++ omarfq/2interacting/todo/todo.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"time"
 )
@@ -45,4 +44,45 @@ func (l *List) Complete(i int) error {
 	ls[i-1].CompletedAt = time.Now()
 
 	return nil
+}
+
+// Delete method deletes a ToDo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	// Adjusting index for 0 based index
+	*l = append(ls[:i-1], ls[i:]...)
+	return nil
+}
+
+// Save method encodes the List as JSON and saves it
+// using the provided file name
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, js, 0644)
+}
+
+// Get method open the provided file name, decodes
+// the JSON data and parses it into a List
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
 }
